73e494927739825f4f92649e60e9a4e303117b6f
Ubuntu <UsuarioSV@MV-006.kbxjhwgr2gruzimispp1x2wiqa.cx.internal.cloudapp.net>
2025-10-27 23:10:19 +0000

    Paso primero

--- app.py
+++ app.py
@@ -8,10 +8,21 @@ app = Flask(__name__)
 def call_app_b():
     try:
         # Llamamos a la app B, que está en localhost:3001
-        response = requests.get('http://mi-app-service-2.stnvivanco92-dev.svc.cluster.local:4000/')
-        return f'Respuesta de app 2: {response.text}', response.status_code
+        response = requests.get('http://mi-servicio-2.adonis-paz-dev.svc.cluster.local:4000/')
+        return f'Respuesta de app B: {response.text}', response.status_code
     except requests.exceptions.ConnectionError:
         return 'Error: no se pudo conectar con app B', 500
+@app.route('/health', methods=['GET'])
+def call_health():
+    return 'OK', 200
+
+@app.route('/startup', methods=['GET'])
+def call_startup():
+    return 'OK', 200
+    
+@app.route('/readiness', methods=['GET'])
+def call_readiness():
+    return 'OK', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
